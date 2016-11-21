b066129f7bcb7b64b8b4b73fa7460e7805e33c4a
AREBLATESS <105021008@live.asia.edu.tw>
2016-11-21 16:22:28 +0800

    ex01.02 OK

--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -2,14 +2,32 @@
 /*
  * Topic: 將前一題加強成可以做10以下的各種數字系統轉換
  * Date: 2016/11/21
- * Author: 1050210XX 周永振老師
+ * Author: 105021008 陳畯程
  */
-
+import java.util.Scanner;
 public class ex02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scn=new Scanner(System.in);
+		System.out.print("輸入 數字,幾進制");
+		int n=scn.nextInt();
+		int m=scn.nextInt();
+		System.out.print(fun(n,m));}
+	public static int fun(int a,int m){
 
+		int sum = 0;
+		int sum2 = 0;
+		int x = 1;
+		
+		while(a>0){
+		sum=a%m;
+		a=a/m;
+		sum2 = sum2+(sum*x);
+		x = x*10;
+		
+		}
+		return sum2;
 	}
-
-}
+	
+	}
